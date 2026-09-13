712e1df4f408d2a5334c4f593e038bd1d8d5412f
JuanForge <contact.juan.coding@gmail.com>
2026-09-14 00:26:25 +0200

    final

--- src/enboite/lib/tooling.py
+++ src/enboite/lib/tooling.py
@@ -106,7 +106,7 @@ def _input_live(*args, y_n: bool=True) -> str|bool:
     except Exception as e:  # noqa: BLE001
         print(e)
     for i in args:
-        print(i)
+        print(str(i)+"n")
     value = input("input requit y/n >" if y_n else "input requit >").strip()
     try:
         if LIVE:
